60c326c0d09700fb5b44187eb6a188e52480f718
Danny Waite <dannywaite7@gmail.com>
2017-09-10 17:32:56 +0100

    add nats subscriber

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const electron = require('electron');
 const path = require('path');
+const NATS = require('nats');
 
 const { app, BrowserWindow } = electron;
 
@@ -18,6 +19,10 @@ const electronConfig = {
   URL_LAUNCHER_ZOOM: parseFloat(process.env.URL_LAUNCHER_ZOOM || 1.0),
   URL_LAUNCHER_OVERLAY_SCROLLBARS: process.env.URL_LAUNCHER_CONSOLE === '1' ? 1 : 0,
 };
+
+let browserWindow;
+let nats;
+let natsEnabled;
 
 // enable touch events if your device supports them
 if (electronConfig.URL_LAUNCHER_TOUCH) {
@@ -44,7 +49,32 @@ if (process.env.NODE_ENV === 'development') {
  */
 app.on('ready', () => {
   // here we actually configure the behavour of electronJS
-  const window = new BrowserWindow({
+
+  loadWindow();
+  loadUrl(electronConfig.URL_LAUNCHER_URL);
+  
+});
+
+if (process.env.NATS_ADDRESS) {
+  var servers = [`nats://${process.env.NATS_ADDRESS}:4222`];
+  this.nats = NATS.connect({'servers': servers});
+
+  this.natsEnabled = true;
+
+  console.log("connected to nats at " + servers[0])
+}
+
+console.log("nats is " + this.natsEnabled)
+
+if (this.natsEnabled) {
+  this.nats.subscribe(process.env.NATS_TOPIC, function(url) {
+    console.log("loading " + url);
+    loadUrl(url);
+  });
+}
+
+function loadWindow() {
+  browserWindow = new BrowserWindow({
     width: electronConfig.URL_LAUNCHER_WIDTH,
     height: electronConfig.URL_LAUNCHER_HEIGHT,
     frame: !!(electronConfig.URL_LAUNCHER_FRAME),
@@ -57,18 +87,21 @@ app.on('ready', () => {
     },
   });
 
-  window.webContents.on('did-finish-load', () => {
+    // if the env-var is set to true,
+  // a portion of the screen will be dedicated to the chrome-dev-tools
+  if (electronConfig.URL_LAUNCHER_CONSOLE) {
+    browserWindow.openDevTools();
+  }
+
+  browserWindow.webContents.on('did-finish-load', () => {
     setTimeout(() => {
-      window.show();
+      browserWindow.show();
     }, 300);
   });
 
-  // if the env-var is set to true,
-  // a portion of the screen will be dedicated to the chrome-dev-tools
-  if (electronConfig.URL_LAUNCHER_CONSOLE) {
-    window.openDevTools();
-  }
+}
 
-  // the big red button, here we go
-  window.loadURL(electronConfig.URL_LAUNCHER_URL);
-});
+function loadUrl(url) {
+    // the big red button, here we go
+    browserWindow.loadURL(url);
+}
